632175d2e094f2601a519ee9d76469f8c2fe349c
Ruan Rocha <ruan.hoinaski@gmail.com>
2023-06-25 14:54:44 -0300

    first commit

---
+++ app/Controllers/Login.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Login extends Controller{
+    public function edit () 
+    {
+        return 'eita';
+    }
+}
---
+++ app/Controllers/Menu.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Menu extends Controller{
+    public function index () {
+        return view('Menu');
+    }
+}
